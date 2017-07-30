e401d45ebb13f9096ff1548f5a2be6c14343679d
Noura <noura@Nouras-MacBook-Pro.local>
2017-07-30 10:35:07 +0300

    Initial Commit

---
+++ خلوتي/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  خلوتي
+//
+//  Created by Noura on 11/7/1438 AH.
+//  Copyright © 1438 King Fahad National Library. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
